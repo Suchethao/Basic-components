c45760f2955463ca9f9465c371c3715765b8cc8f
suchethaleonidou <Suchethao>
2023-02-21 12:02:00 -0500

    adding some blank form components- styling to come

--- btn-storybook/src/Components/Forms.js
+++ btn-storybook/src/Components/Forms.js
@@ -11,11 +11,38 @@ const Forms = ({ size }) => {
   return (
     <div>
       <form>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" name="name" /><br />
+        <div>
+        <label htmlFor="name">Email:</label>
+        </div>
+        <input type="text" id="name" name="name" placeholder='Email' /><br />
+      </form>
+      <form>
+        <div>
+        <label htmlFor="name">Select Input:</label>
+        </div>
+        <input type="text" id="name" name="name" placeholder='Select Input' /><br />
+      </form>
+      <form>
+        <div>
+        <label htmlFor="name">Number Input:</label>
+        </div>
+        <input type="text" id="name" name="name" placeholder='Number Input' /><br />
+      </form>
+      <form>
+        <div>
+        <label htmlFor="name">Text Input with Button:</label>
+        </div>
+        <input type="text" id="name" name="name" placeholder='Text Input with Button' /><br />
+      </form>
+      <form>
+        <div>
+        <label htmlFor="name">Checkbox:</label>
+        </div>
+        <input type="text" id="name" name="name" placeholder='Checkbox' /><br />
       </form>
     </div>
   );
 };
+
 
 export default Forms;
